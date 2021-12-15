21557c1f5a5360e3430b1c24482c5513744e9d11
Jelle Spijker <j.spijker@ultimaker.com>
2021-12-15 09:50:29 +0100

    Bumped up version to 5.0.0

--- conanfile.py
+++ conanfile.py
@@ -10,7 +10,7 @@ required_conan_version = ">=1.42"
 
 class CuraEngineConan(ConanFile):
     name = "curaengine"
-    version = "99.9.2-alpha+001"
+    version = "5.0.0"
     license = "AGPL-3.0"
     author = "Ultimaker B.V."
     url = "https://github.com/Ultimaker/CuraEngine"
@@ -22,6 +22,8 @@ class CuraEngineConan(ConanFile):
     default_user = "ultimaker"
     default_channel = "testing"
     exports = "LICENSE*"
+    python_requires = ["UltimakerBase/0.1@ultimaker/testing"]
+    python_requires_extend = "UltimakerBase.UltimakerBase"
     options = {
         "enable_arcus": [True, False],
         "enable_openmp": [True, False],
@@ -58,7 +60,7 @@ class CuraEngineConan(ConanFile):
     def requirements(self):
         self.requires("stb/20200203")
         if self.options.enable_arcus:
-            self.requires(f"arcus/4.13.0-alpha+001@ultimaker/testing")
+            self.requires(f"arcus/[~=5.0.0-a]@ultimaker/testing")
             self.requires("protobuf/3.17.1")
         self.requires("clipper/6.4.2")
         self.requires("rapidjson/1.1.0")
