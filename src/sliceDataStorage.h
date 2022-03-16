7c1e771bc6701404e155a639cc13aeaf8a8ec719
Tim Kuipers <t.kuipers@ultimaker.com>
2022-03-16 17:41:39 +0100

    Print support brim.
    The support brim insets are added without order constraints.
    Ordering them would take more computation time and make the code more complex.
    We don't need to print them in any specific order because for the first layer of support
    we don't really care about accuracy.
    
    Moreover, the order optimizer will choose an efficient path and so most often the support brim will be printed from outside inward,
    which is what you would want because support is open on the inside and closed by normal brim on the outside.
    This way the overextrudate is pushed toward where there is room for it: the relatively open support infill.

--- src/sliceDataStorage.h
+++ src/sliceDataStorage.h
@@ -323,7 +323,7 @@ public:
     SupportStorage support;
     
     std::vector<SkirtBrimLine> skirt_brim[MAX_EXTRUDERS]; //!< Skirt/brim polygons per extruder, ordered from inner to outer polygons.
-    std::vector<SkirtBrimLine> tower_brim; //!< brim lines for the tower
+    Polygons support_brim; //!< brim lines for support, going from the edge of the support inward. \note Not ordered by inset.
     Polygons raftOutline;               //Storage for the outline of the raft. Will be filled with lines when the GCode is generated.
 
     int max_print_height_second_to_last_extruder; //!< Used in multi-extrusion: the layer number beyond which all models are printed with the same extruder
